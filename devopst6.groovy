6fce512f9866052edbfd47ac5d8ec1e6e401e042
GaneshSaini <51072593+GaneshSaini@users.noreply.github.com>
2020-08-13 01:33:38 +0530

    Update devopst6.groovy

--- devopst6.groovy
+++ devopst6.groovy
@@ -1,48 +1,48 @@
   
-job("dev-t6-job1"){
+job("job1"){
   description("Pull files from github repo automatically when some developers push code to github")
   scm{
-    github("Priyanshi-Garg/dev-task6-code","master")
+    github("GaneshSaini/devopstask6","master")
   }
   triggers {
     scm("* * * * *")
   }
   steps{
-    shell('''if ls / | grep task6-ws
+    shell('''if ls / | grep devopstask6-ws
 then
-sudo cp -rf * /task6-ws
+sudo cp -rf * /devopstask6-ws
 else
-sudo mkdir /task6-ws
-sudo cp -rf * /task6-ws
+sudo mkdir /devopstask6-ws
+sudo cp -rf * /devopstask6-ws
 fi  
 ''')
   }
 }
 
-job("dev-t6-job2"){
+job("job2"){
   description("By looking at the code or program file, Jenkins should automatically start the respective language interpreter installed image container to deploy code on top of Kubernetes ( eg. If code is of PHP, then Jenkins should start the container that has PHP already installed ) .")
   
   authenticationToken('deploy')
   
   triggers {
-    upstream("dev-t6-job1", "SUCCESS")
+    upstream("job1", "SUCCESS")
   }
   steps{
-shell('''cd /task6-ws
+shell('''cd /devopstask6-ws
 if ls | grep ".html"
 then
 if ls | grep ws-html
 then
-sudo rm -rvf /task6-ws/ws-html
-sudo mkdir /task6-ws/ws-html
+sudo rm -rvf /devopstask6-ws/ws-html
+sudo mkdir /devopstask6-ws/ws-html
 else
-sudo mkdir /task6-ws/ws-html
+sudo mkdir /devopstask6-ws/ws-html
 fi
-sudo cp -rvf /task6-ws/*.html /task6-ws/ws-html
-sudo kubectl delete -f task-6.yml
-sudo kubectl create -f task-6.yml
+sudo cp -rvf /devopstask6-ws/*.html /task6-ws/ws-html
+sudo kubectl delete -f devopstask-6.yml
+sudo kubectl create -f devopstask-6.yml
 sleep 20
-cd /task6-ws/ws-html
+cd /devopstask6-ws/ws-html
 ls
 sshpass -p "tcuser" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r * docker@192.168.99.105:/home/docker/devops-task6
 fi 
@@ -50,10 +50,10 @@ fi
   }
 }
 
-job("dev-t6-job3"){
+job("job3"){
     description("Testing JOB")
 	triggers{
-		upstream('dev-t6-job2' , 'SUCCESS')
+		upstream('job2' , 'SUCCESS')
 	}
 	steps{
 		shell('''status=$(curl -o /dev/null  -s  -w "%{http_code}"  http://192.168.99.105:30000)
@@ -67,11 +67,11 @@ fi
  }
 }
 
-job("dev-t6-job4 "){
+job("job4 "){
   description("If app is not working , then send email to developer with error messages and redeploy the application after code is being edited by the developer .")
 
 triggers {
-    upstream("dev-t6-job3", "SUCCESS")
+    upstream("job3", "SUCCESS")
   }
   steps{
     shell('''
@@ -80,7 +80,7 @@ triggers {
     echo "Everything is fine"
     else
     sudo python3 /root/mail.py
-    sudo curl -I --user admin:<password> http://192.168.99.102:8080//job/dev-t6-job2/build?token=deploy
+    sudo curl -I --user admin:<password> http://192.168.99.102:8080//job/devopstask6-job2/build?token=deploy
     fi
 ''')
 }
@@ -91,7 +91,7 @@ buildPipelineView("Pipeline_Of_task-6") {
     filterExecutors(false)
     title("Task-6")
     displayedBuilds(1)
-    selectedJob("dev-t6-job1")
+    selectedJob("job1")
     alwaysAllowManualTrigger(true)
     showPipelineParameters(true)
     refreshFrequency(5)
